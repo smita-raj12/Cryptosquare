2869b9f851dce8a52a71729fb7e5b37ec4a44980
smita Raj <smita.raj12@gmail.com>
2021-06-13 11:46:53 -0700

    created project on cryptosquare

---
+++ README.md
@@ -0,0 +1,5 @@
+## Cryptosquare(text)
+
+**Test:** "don't compare yourself to others, compare yourself to   the person you were yesterday" 
+**code:** Cryptosquare("don't compare yourself to others, compare yourself to   the person you were yesterday" )
+**Expeted output:** "daeer leweo rlref rerne fsyts rdtyt coooe acooo utnyy ouomr hyemr tpseo spsha eput"
---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+function Cryptosquare(text){
+  let strArray= text.split("");
+  let criptoStr = ' '
+  for(let j=0;j<=(strArray.length/8);j++){
+    criptoStr = criptoStr + ' '
+    for(let i=j;i<=strArray.length-1;i=i+8){
+      criptoStr = criptoStr + strArray[i];
+    }
+  }
+  console.log("test",criptoStr)
+}
+
+$(document).ready(function() {
+  $("#formCry").submit(function(event){
+    event.preventDefault();
+    const UserInput = $("#user-input").val();
+    console.log("user-input",UserInput)
+    const passage = Cryptosquare(UserInput);
+    console.log("passage",passage);
+    $("#output").html(passage);
+  });
+});
+
